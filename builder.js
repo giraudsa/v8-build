00b7813accc467ad1acbdb56c3191b502f0f5673
hehao <hehao@wedobest.com.cn>
2025-11-05 15:13:20 +0800

    chore: update build configurations for Android and iOS to enable WebAssembly and adjust pointer compression settings

---
+++ builder.js
@@ -0,0 +1,105 @@
+
+/**
+/**
+ * 用途：构建脚本相关的工具函数及流程控制
+ */
+
+const fs = require("fs");
+const path = require("path");
+
+const trace = function (...args) {
+    console.log("builder: ", ...args);
+}
+
+trace(process.env);
+
+/**
+ * 解析args.gn文件，返回一个对象
+ * @param {*} argsPath args.gn文件路径
+ * @returns 对象
+ */
+let parseGNArgs = function (argsPath) {
+    const content = fs.readFileSync(argsPath, "utf8");
+    const lines = content.split("\n");
+    const args = {};
+    for (let line of lines) {
+        const trimmedLine = line.trim();
+        if (trimmedLine.length === 0 || trimmedLine.startsWith("#")) {
+            continue;
+        }
+        if (trimmedLine.includes("=")) {
+            const [key, value] = trimmedLine.split("=");
+            args[key] = value.trim();
+        }
+    }
+    return args;
+}
+const v8Version = process.env.V8_VERSION;
+const platform = process.env.PLATFORM; // arm64|arm|x64
+const jobName = process.env.JOB_NAME;  // android|ios|mac|win
+const workspace = process.env.WORKSPACE;
+
+const v8SourcePath = path.join(workspace, "v8");
+
+let onBeforeBuild = function () {
+    switch (jobName) {
+        case "android":
+
+            break;
+        case "ios": {
+            // parse args.ios.${{ matrix.platform }}.gn
+            const argsPath = path.join(workspace, `args.ios.${platform}.gn`);
+            const args = parseGNArgs(argsPath);
+            trace("v8_enable_webassembly=" + args["v8_enable_webassembly"]);
+            if (args["v8_enable_webassembly"] === "true") {
+                {
+                    //  BUILD.gn
+                    let gnPath = path.join(v8SourcePath, "BUILD.gn");
+                    let content = fs.readFileSync(gnPath, "utf8");
+                    const lines = content.split("\n");
+                    let isModify = false;
+                    for (let i = 0; i < lines.length; i++) {
+                        if (lines[i].includes("is_drumbrake_supported")) {
+                            trace("Modify BUILD.gn:line-" + (i - 1));
+                            lines[i - 1] = `# ${lines[i - 1]}`;
+                            isModify = true;
+                            break;
+                        }
+                    }
+                    isModify && fs.writeFileSync(gnPath, lines.join("\n"));
+                }
+                {
+                    // v8/gni/v8.gni
+                    const codes = `      
+      v8_enable_lite_mode = true
+      # Since v8 has the Wasm interpreter mode with jitless, iOS enables it
+      # to use WebAssembly.
+      v8_enable_drumbrake = true
+      v8_enable_webassembly = true
+
+      # iOS doesn't use Turbofan.
+      v8_enable_turbofan = false
+`;
+                    let gniPath = path.join(v8SourcePath, "gni", "v8.gni");
+                    let content = fs.readFileSync(gniPath, "utf8");
+                    const lines = content.split("\n");
+                    let isModify = false;
+                    for (let i = 0; i < lines.length; i++) {
+                        if (lines[i].includes("use_blink")) {
+                            trace("Modify gni/v8.gn:line-" + (i));
+                            lines[i] = lines[i] + codes;
+                            break;
+                        }
+                    }
+                    isModify && fs.writeFileSync(gniPath, lines.join("\n"));
+                }
+            }
+            break;
+        }
+
+        case "mac":
+
+            break;
+    }
+};
+onBeforeBuild();
